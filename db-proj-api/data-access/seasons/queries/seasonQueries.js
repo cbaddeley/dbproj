4a778bcac850bb37d2d4a21063fbf947b84df209
dudescott <scott.engelhardt7@gmail.com>
2021-04-17 09:38:47 -0400

    Update seasonQueries.js

--- db-proj-api/data-access/seasons/queries/seasonQueries.js
+++ db-proj-api/data-access/seasons/queries/seasonQueries.js
@@ -2,35 +2,41 @@ const getSeasonalQuartiles = `
 SELECT yr, season, AVG(avg_rating) AS avg_rating, AVG(roi) AS roi
 FROM (
     WITH all_results as  ( 
-        SELECT imdb_id, yr, season, AVG(avg_rating) AS avg_rating, rating_rank, AVG(roi) AS roi, roi_rank
-        FROM (
-            SELECT imdb_id, avg_rating, rating_rank, roi, roi_rank,
-                EXTRACT(YEAR FROM release_date) AS yr, 
-                CEIL((MOD(EXTRACT(MONTH FROM release_date), 12) + 1) / 3) AS season -- Winter = 1 Spring = 2 Summer = 3 Fall = 4
+        SELECT imdb_id, yr, season, ((SUM(revenue) / SUM(budget)) * 100) AS roi, 
+            RANK() OVER (ORDER BY ((SUM(revenue) / SUM(budget)) * 100)) AS roi_rank, 
+            AVG(avg_rating) AS avg_rating, RANK() OVER (ORDER BY AVG(avg_rating)) AS rating_rank
+        FROM
+        (
+            SELECT imdb_id, yr, season, AVG(avg_rating) AS avg_rating, sum(revenue) as revenue, sum(budget) as budget
             FROM (
-                SELECT *
+                SELECT imdb_id, avg_rating, revenue, budget,
+                    EXTRACT(YEAR FROM release_date) AS yr, 
+                    CEIL((MOD(EXTRACT(MONTH FROM release_date), 12) + 1) / 3) AS season -- Winter = 1 Spring = 2 Summer = 3 Fall = 4
                 FROM (
-                    SELECT imdb_id, AVG(avg_rating) AS avg_rating, RANK() OVER (ORDER BY AVG(avg_rating)) AS rating_rank
+                    SELECT *
                     FROM (
-                        SELECT imdb_id, AVG(Rating) AS avg_rating
-                        FROM Movie NATURAL JOIN RATING
-                        Group by imdb_id
+                        SELECT imdb_id, avg_rating
+                        FROM (
+                            SELECT imdb_id, AVG(Rating) AS avg_rating
+                            FROM Movie NATURAL JOIN RATING
+                            Group by imdb_id
+                        )
                     )
-                    GROUP BY imdb_id
-                )
-            ) 
-            NATURAL FULL OUTER JOIN ( -- needed bc not all movies have ROI so we don't want their rating to drop
-                SELECT *
-                FROM (
-                    SELECT imdb_id, release_date, (revenue / budget) * 100 AS roi,  RANK() OVER (ORDER BY (revenue / budget) * 100) AS roi_rank
-                    FROM  Movie
-                    WHERE budget <> 0 AND revenue <> 0
                 ) 
-                GROUP BY imdb_id, release_date, roi, roi_rank     
-            )
-        WHERE release_date BETWEEN TO_DATE(:startDate, 'YYYY-MM-DD') AND TO_DATE(:endDate, 'YYYY-MM-DD') -- start and end passed from user 
-        )   
-        GROUP BY imdb_id, yr, season, rating_rank, roi_rank
+                NATURAL FULL OUTER JOIN ( -- needed bc not all movies have ROI so we don't want their rating to drop
+                    SELECT *
+                    FROM (
+                        SELECT imdb_id, release_date, sum(revenue) as revenue, sum(budget) as budget
+                        FROM  Movie
+                        WHERE budget <> 0 AND revenue <> 0
+                        GROUP BY imdb_id, release_date
+                    )  
+                )
+            WHERE release_date BETWEEN TO_DATE(:startDate, 'YYYY-MM-DD') AND TO_DATE(:endDate, 'YYYY-MM-DD')
+            )   
+            GROUP BY imdb_id, yr, season
+        )
+        GROUP BY imdb_id, yr, season    
     )
     (
         -- ratings in top 25%
@@ -76,7 +82,7 @@ FROM (
             ) * 0.25
     )
 )
-WHERE quartile = :quartile
+WHERE quartile = :quartile AND yr < EXTRACT(YEAR FROM TO_DATE(:endDate, 'YYYY-MM-DD')) + 1
 GROUP BY yr, season
 ORDER BY yr, season
 `;
